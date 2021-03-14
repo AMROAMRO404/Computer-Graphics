a0a4a78929f961c8848360935f93b4cde1a63930
AMROAMRO404 <amro.amro1999@gmail.com>
2021-03-14 16:03:48 +0200

    add case 1 in bresenham algorithm

--- RenderWidget.cpp
+++ RenderWidget.cpp
@@ -42,9 +42,9 @@ void RenderWidget::paintEvent(QPaintEvent *)
   }*/
 
   //horizental lines
-  myDrawLine(250, 350, 292, 350);
-  myDrawLine(465, 350, 500, 350);
-  myDrawLine(250, 380, 500, 380);
+  myDrawLineB(250, 350, 292, 350);
+  myDrawLineB(465, 350, 500, 350);
+  myDrawLineB(250, 380, 500, 380);
 
   //vertical lines
   myDrawLine(250, 350, 250, 380);
@@ -52,8 +52,8 @@ void RenderWidget::paintEvent(QPaintEvent *)
 
 
   //horizental lines
-  myDrawLine(260, 260, 370, 260);
-  myDrawLine(260, 280, 370, 280);
+  myDrawLineB(260, 260, 370, 260);
+  myDrawLineB(260, 280, 370, 280);
 
   //vertical lines
   myDrawLine(260, 260, 260, 280);
@@ -95,7 +95,33 @@ void RenderWidget::paintEvent(QPaintEvent *)
   myDrawCircle(380,320,15,360);
 }
 
-//REFERENCE: https://stackoverflow.com/questions/10060046/drawing-lines-with-bresenhams-line-algorithm
+void RenderWidget::myDrawLineB(float x1, float y1, float x2, float y2)
+{
+  QPainter painter(this);
+
+  //    painter.setRenderHint(QPainter::Antialiasing, false);
+  QColor color(0, 0, 0);
+  painter.setPen(color);
+  QPen pen(Qt::black);
+  pen.setWidth(4);
+  painter.setPen(pen);
+  int dx,dy,y,p;
+  painter.drawPoint(x1, y1);
+  y = y1;
+  dx = x2 - x1;
+  dy = y2 - y1;
+  p = 2*dy - dx;
+  for(int x = x1+1; x <= x2; ++x) {
+      if (p<0) {
+         painter.drawPoint(x, y);
+         p = p + 2*dy;
+      } else {
+          y = y + 1;
+          painter.drawPoint(x, y);
+          p = p + 2*dy - 2*dx;
+        }
+  }
+}
 void RenderWidget::myDrawLine(float x1, float y1, float x2, float y2)
 {
   QPainter painter(this);
@@ -106,74 +132,35 @@ void RenderWidget::myDrawLine(float x1, float y1, float x2, float y2)
   QPen pen(Qt::black);
   pen.setWidth(4);
   painter.setPen(pen);
-  //float m = (y2 - y1) / (x2 - x1);
-  int x,y,dx,dy,dx1,dy1,px,py,xe,ye,i;
-   dx=x2-x1;
-   dy=y2-y1;
-   dx1=fabs(dx);
-   dy1=fabs(dy);
-   px=2*dy1-dx1;
-   py=2*dx1-dy1;
-   if(dy1<=dx1) {
-    if(dx>=0) {
-     x=x1;
-     y=y1;
-     xe=x2;
-    }
-    else {
-     x=x2;
-     y=y2;
-     xe=x1;
-    }
-    painter.drawPoint(x, y);
-    for(i=0;x<xe;i++) {
-     x=x+1;
-     if(px<0) {
-      px=px+2*dy1;
-     }
-     else {
-      if((dx<0 && dy<0) || (dx>0 && dy>0)) {
-       y=y+1;
-      }
-      else {
-       y=y-1;
-      }
-      px=px+2*(dy1-dx1);
-     }
-     painter.drawPoint(x, y);
-    }
-   }
-   else {
-    if(dy>=0) {
-     x=x1;
-     y=y1;
-     ye=y2;
-    }
-    else {
-     x=x2;
-     y=y2;
-     ye=y1;
-    }
-    painter.drawPoint(x, y);
-    for(i=0;y<ye;i++) {
-     y=y+1;
-     if(py<=0) {
-      py=py+2*dx1;
-     }
-     else {
-      if((dx<0 && dy<0) || (dx>0 && dy>0)) {
-       x=x+1;
-      }
-      else {
-       x=x-1;
-      }
-      py=py+2*(dx1-dy1);
-     }
-     painter.drawPoint(x, y);
-    }
-   }
-}
+  float m = (y2 - y1) / (x2 - x1);
+  //  SetPixel(x1, y1, color);   // first point
+  painter.drawPoint(x1, y1);
 
+  if (m < 1)
+  {
+    float y = y1;
+    for (int i = x1 + 1; i < x2; ++i)
+    {
+      y = y + m;
+      //      SetPixel(i, round(y), color);
+      painter.drawPoint(i, round(y));
+    }
+  }
+  else     // i.e., m > 1
+  {
+    float mm = 1 / m;
+    float x = x1;
+    for (int i = y1 + 1; i < y2; ++i)
+    {
+      x = x + mm;
+      //      SetPixel(round(x), i, color);
+      painter.drawPoint(round(x), i);
+    }
+  }
+
+  //  SetPixel(x2, y2, color);   // last point
+  painter.drawPoint(x2, y2);
+}
 void RenderWidget::myDrawCircle(float Xc, float Yc, float r, float angle)
 {
     QPainter painter(this);
@@ -192,7 +179,6 @@ void RenderWidget::myDrawCircle(float Xc, float Yc, float r, float angle)
         painter.drawPoint(x, y);
         theta+=0.1;
     }
-
 }
 
 void RenderWidget::myDrawArc(float Xc, float Yc, float t1, float t2, float r){
@@ -212,5 +198,4 @@ void RenderWidget::myDrawArc(float Xc, float Yc, float t1, float t2, float r){
         painter.drawPoint(x, y);
         theta+=(1/r);
     }
-
 }
--- RenderWidget.h
+++ RenderWidget.h
@@ -22,7 +22,8 @@ public:
 	QSize minimumSizeHint() const override;
 	QSize sizeHint() const override;
 
-	void myDrawLine(float x1, float y1, float x2, float y2);
+    void myDrawLineB(float x1, float y1, float x2, float y2);
+    void myDrawLine(float x1, float y1, float x2, float y2);
     void myDrawCircle(float Xc,float Yc, float r, float angle);
     void myDrawArc(float Xc,float Yc, float t1,float t2, float r);
 protected:
